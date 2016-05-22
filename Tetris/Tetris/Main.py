00f8c77c7717749c8692c91847154821bb4d7e3f
jayjihun <t080205@gmail.com>
2016-05-22 17:33:34 +0900

    Save / Load 구현 완료

--- Tetris/Tetris/Main.py
+++ Tetris/Tetris/Main.py
@@ -1,11 +1,24 @@
 ﻿#from TetrisCore import *
 from NewTetrisCore import *
 
+def play_history(filename):
+	folder='C:/Tetris/'
+	full = folder+filename
+	app = wx.App()
+	Tetris(
+		inputDevice=Board.INPUT_SAVE,
+		inputFile=full
+		)
+	app.MainLoop()
+
+def human_play():
+	app = wx.App()
+	Tetris()
+	app.MainLoop()
 
 def main():
-	app = wx.App()
-	Tetris(None, title='Tetris')
-	app.MainLoop()
+	#human_play()
+	play_history('History_13_2016-05-22_17-29-47-653743.sav')
 
 if __name__=='__main__':
 	main()
